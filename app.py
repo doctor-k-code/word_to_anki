56b36fbc443484fa6a1417e13c93bd6c2e2a2823
doctor-k-code <info@doctor-k.net>
2024-04-28 15:40:33 +0900

    2024/4/28

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+[name] app.py
+[purpose] word to csv for anki
+[references]
+
+written by Dr.K, 2024/4/28
+"""
+import datetime
+import glob
+import os
+import shutil
+
+import docx
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+st.title("wordファイルをanki用のCSVファイルに変換します")
