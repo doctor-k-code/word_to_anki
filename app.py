70dbd17ecdf9ca9ed0a9a105bb280e5df06ec1e0
doctor-k-code <info@doctor-k.net>
2024-04-28 21:45:21 +0900

    2024/4/26

--- app.py
+++ app.py
@@ -101,9 +101,12 @@ st.code(back_temp)
 '''
 
 st.divider()
-'''
-### このサイトの製作者
-* [ブログ](https://doctor-k.net)もよろしくお願いします
-* 作成したコードは[こちら](https://github.com/doctor-k-code)
-'''
-st.image(ICON_PATH, width=10, caption="駆け出し医師Dr.K")
+st.write('### このサイトの製作者')
+left, right = st.columns(2)
+with left:
+    st.image(ICON_PATH, width=700, caption="駆け出し医師Dr.K")
+with right:
+    '''
+    * [ブログ](https://doctor-k.net)もよろしくお願いします
+    * 作成したコードは[こちら](https://github.com/doctor-k-code)
+    '''
