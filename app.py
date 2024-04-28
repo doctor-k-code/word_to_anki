7c266549314124202138303ac736f67c8d8b787d
doctor-k-code <info@doctor-k.net>
2024-04-28 16:09:34 +0900

    2024/4/28

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 [name] app.py
 [purpose] word to csv for anki
 [reference]
+    https://docs.streamlit.io/develop/api-reference
 
 written by Dr.K, 2024/4/28
 """
@@ -17,3 +18,10 @@ import streamlit as st
 
 
 st.title("wordファイルをanki用のCSVファイルに変換します")
+
+uploaded_files = st.file_uploader(
+    "wordファイルをアップロードして下さい。", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    byte_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    
