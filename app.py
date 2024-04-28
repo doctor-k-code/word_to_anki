43bc646fad6349b78e117e0ae634af3c00b6f224
doctor-k-code <info@doctor-k.net>
2024-04-28 17:30:13 +0900

    2024/4/28

--- app.py
+++ app.py
@@ -22,13 +22,16 @@ uploaded_files = st.file_uploader(
     "wordファイルをアップロード", type="docx", accept_multiple_files=True)
 
 if st.button("実行"):
-    time_stamp = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-    time_stamp = time_stamp.strftime('%m%d%H%M')
-    table = None
-    for uploaded_file in uploaded_files:
-        table = file_to_table(uploaded_file, table=table, title=title)
-    st.dataframe(table)
+    if title is None:
+        st.write("タイトルを入力して下さい。")
+    else:
+        time_stamp = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+        time_stamp = time_stamp.strftime('%m%d%H%M')
+        table = None
+        for uploaded_file in uploaded_files:
+            table = file_to_table(uploaded_file, table=table, title=title)
+        st.dataframe(table)
 
-    result_csv = table_to_csv(table)
-    file_name = '_'.join(['for_anki', time_stamp+'.csv'])
-    st.download_button(label="ダウンロード", data=result_csv, file_name=file_name)
+        result_csv = table_to_csv(table)
+        file_name = '_'.join(['for_anki', time_stamp+'.csv'])
+        st.download_button(label="ダウンロード", data=result_csv, file_name=file_name)
